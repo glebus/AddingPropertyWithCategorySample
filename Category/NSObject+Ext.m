0c96e07c09ac82ae48afcfb0706853f5065f665c
Gleb Ustimenko <gleb.ustimenko@injoit.com>
2013-02-13 13:36:14 +0200

    Added some comments.

--- Category/NSObject+Ext.m
+++ Category/NSObject+Ext.m
@@ -16,11 +16,23 @@ static NSString *NSOBJECT_TEST_PROPERTY_KEY = @"com.domus.nsobject.test";
 
 @dynamic test;
 
+/**
+ * Overriding setter for property test. Use method objc_setAssociatedObject for associated self with object "test" in runtime.
+ *
+ * @param [in,out]	NSString*   Variable for property test.
+ *
+ * @return	void
+ **/
 - (void)setTest:(NSString *)test
 {
     objc_setAssociatedObject(self, (__bridge const void *)NSOBJECT_TEST_PROPERTY_KEY, test, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
+/**
+ * Overriding getter for property test. Use method objc_getAssociatedObject getting associated object "test" in runtime.
+ *
+ * @return	NSString*   Variable for property test.
+ **/
 - (NSString *)test
 {
     return objc_getAssociatedObject(self, (__bridge const void *)NSOBJECT_TEST_PROPERTY_KEY);
