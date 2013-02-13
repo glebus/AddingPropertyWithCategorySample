f279842cd7d89b69061ee26645976a261d3d17c1
Gleb Ustimenko <gleb.ustimenko@injoit.com>
2013-02-13 12:50:04 +0200

    Initial Commit

---
+++ AddingPropertyWithCategorySampleTests/AddingPropertyWithCategorySampleTests.h
@@ -0,0 +1,13 @@
+//
+//  AddingPropertyWithCategorySampleTests.h
+//  AddingPropertyWithCategorySampleTests
+//
+//  Created by Gleb Ustimenko on 2/13/13.
+//  Copyright (c) 2013 Gleb Ustimenko. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AddingPropertyWithCategorySampleTests : SenTestCase
+
+@end
---
+++ AddingPropertyWithCategorySampleTests/AddingPropertyWithCategorySampleTests.m
@@ -0,0 +1,32 @@
+//
+//  AddingPropertyWithCategorySampleTests.m
+//  AddingPropertyWithCategorySampleTests
+//
+//  Created by Gleb Ustimenko on 2/13/13.
+//  Copyright (c) 2013 Gleb Ustimenko. All rights reserved.
+//
+
+#import "AddingPropertyWithCategorySampleTests.h"
+
+@implementation AddingPropertyWithCategorySampleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in AddingPropertyWithCategorySampleTests");
+}
+
+@end
